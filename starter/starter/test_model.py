6d80cb2553d4572a14da94715de964055e0cc83e
Arian Afzaly <arian.afzaly@live.de>
2023-03-28 15:50:32 +0200

    create, train and test ml model

---
+++ starter/starter/test_model.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import numpy as np
+import pytest
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+file_dir = os.path.dirname(__file__)
+sys.path.insert(0, file_dir)
+
+from ml.model import train_model, compute_model_metrics, inference
+
+# Define a fixture that creates sample data for testing
+@pytest.fixture(scope='module')
+def sample_data():
+    # Generate a random dataset for a classification problem
+    X, y = make_classification(n_samples=100, n_features=20, random_state=42)
+    # Split the dataset into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+# Test the train_model function to ensure model training is successful
+def test_train_model(sample_data):
+    X_train, _, y_train, _ = sample_data
+    model = train_model(X_train, y_train)
+    assert model is not None, "Model training failed."
+    assert hasattr(model, "predict"), "Model should have a predict method."
+
+
+# Test the compute_model_metrics function to ensure it returns valid metrics
+def test_compute_model_metrics(sample_data):
+    _, X_test, _, y_test = sample_data
+    X_train, y_train = X_test.copy(), y_test.copy()
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    assert 0 <= precision <= 1, "Precision should be between 0 and 1."
+    assert 0 <= recall <= 1, "Recall should be between 0 and 1."
+    assert 0 <= fbeta <= 1, "F1 score should be between 0 and 1."
+
+
+# Test the inference function to ensure it returns predictions with the correct format
+def test_inference(sample_data):
+    X_train, X_test, y_train, _ = sample_data
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+
+    assert len(preds) == len(X_test), "Number of predictions should match the number of test samples."
+    assert set(preds).issubset({0, 1}), "Predictions should be either 0 or 1."
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,28 +1,45 @@
 # Script to train machine learning model.
 
+# Import necessary modules
+import os
+import sys
+import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+# Import custom functions
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
 
-# Add code to load in the data.
+# Define file paths
+file_dir = os.path.dirname(__file__)
+data_path = os.path.join(file_dir, '../data/clean_census.csv')
+model_path = os.path.join(file_dir, '../model/rf_model.pkl')
+encoder_path = os.path.join(file_dir, '../model/encoder.pkl')
+lb_path = os.path.join(file_dir, '../model/lb.pkl')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+# Load data
+data = pd.read_csv(data_path)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+# Split data into training and testing sets
+train_data, test_data = train_test_split(data, test_size=0.20)
 
-# Proces the test data with the process_data function.
+# Process training data with custom function
+cat_features = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
+X_train, y_train, encoder, lb = process_data(train_data, categorical_features=cat_features, label="salary", training=True)
 
-# Train and save a model.
+# Process testing data with custom function
+X_test, y_test, _, _ = process_data(test_data, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
+
+# Train and save model
+rf_model = train_model(X_train, y_train)
+pickle.dump(rf_model, open(model_path, 'wb'))
+pickle.dump(encoder, open(encoder_path, 'wb'))
+pickle.dump(lb, open(lb_path, 'wb'))
+
+# Make predictions on test data and compute metrics
+preds = inference(rf_model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+# Print metrics
+print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F-beta score: {fbeta:.4f}")
